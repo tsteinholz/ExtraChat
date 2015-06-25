ef7c6f15e65263090fffc95c4ed8263c65759186
Buuz135 <buuz135@hotmail.com>
2015-06-25 10:39:59 +0200

    Fixed typo #34

--- src/main/java/com/buuz135/extrachat/perms/PermsUtils.java
+++ src/main/java/com/buuz135/extrachat/perms/PermsUtils.java
@@ -7,7 +7,7 @@ public class PermsUtils {
     public static final String FORMATMES = "extrachat.command.formatmes";
     public static final String TAGADD = "extrachat.command.tagadd";
     public static final String TAGREMOVE = "extrachat.command.tagremove";
-    public static final String FORMATTAG = "extrachar.command.formattag";
+    public static final String FORMATTAG = "extrachat.command.formattag";
     public static final String TOGGLELOG = "extrachat.command.togglelog";
     public static final String MAIN = "extrachat.command.*";
     public static final String SHOW = "extrachat.command.show";
